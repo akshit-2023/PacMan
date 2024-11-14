88e081052e9349a319e45118ee5c6e79dd722a42
Akshit <akshitv2004@gmail.com>
2024-11-14 14:31:40 +0530

    Add Graphics

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount=21;
+        int columnCount=19;
+        int tileSize=32;
+        int boardWidth=columnCount*tileSize;
+        int boardHeight=rowCount*tileSize;
+
+        JFrame frame=new JFrame("Pac Man");
+        
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);//Center the frame
+        frame.setResizable(false);//Frame is not resizable
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame=new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
